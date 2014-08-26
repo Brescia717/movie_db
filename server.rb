05d104baf09663ce570d1a0c9e28f7877234afdf
Alex Cornford <amc42294@gmail.com>
2014-08-26 12:04:03 -0400

    Added some stuff

--- server.rb
+++ server.rb
@@ -1,13 +1,23 @@
-require 'Gemfile'
+# require 'Gemfile'
+require  'sinatra'
+require  'sinatra/reloader'
+require  'pg'
+require  'pry'
 
 
 def db_connection
+  @db_info = []
   begin
-    @db_info = []
     @db_info = PG.connect(dbname: 'movies')
-
-    yield(@db_info)
   ensure
     @db_info.close
   end
+end
+
+get '/actors' do
+  @actors = Array.new
+  db_connection do |conn|
+   @actors =  conn.exec('SELECT * FROM actors')
+  end
+  erb :actors
 end
